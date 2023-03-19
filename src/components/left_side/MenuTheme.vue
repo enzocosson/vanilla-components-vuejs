2436b7668b40599f0e6d814bd42779b315ba08f0
enzocosson <enzolemercier@gmail.com>
2023-03-20 00:33:21 +0100

    installation sass et ajout v-btn dans le Menutheme

--- src/components/left_side/MenuTheme.vue
+++ src/components/left_side/MenuTheme.vue
@@ -2,15 +2,29 @@
 
 <template>
   <div class="menu">
-    <v-btn> Front-End </v-btn>
-    <v-btn> Back-End </v-btn>
+    <v-btn class="btn"> Front-End </v-btn>
+    <v-btn class="btn"> Back-End </v-btn>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .menu {
   width: 100%;
   height: 10%;
   background-color: var(--orange);
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+
+  .btn {
+    height: 4vh;
+    background-color: var(--white);
+    color: var(--orange);
+    font-family: var(--font-primary);
+    font-size: 1vw;
+    font-weight: bold;
+    border-radius: 0.2vw;
+  }
 }
 </style>
